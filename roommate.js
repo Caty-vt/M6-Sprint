d5ff89c3af7eadf132c328fac1278406f89387b7
SphinxGo <go.beltran.87@gmail.com>
2023-07-26 22:43:15 -0400

    cambios dia 26 julio

--- roommate.js
+++ roommate.js
@@ -1,23 +1,46 @@
 import { v4 as uuid4 } from "uuid";
 import axios from "axios";
-//import archivo from "fs"
+import petro from "fs"
+import { log } from "console";
+
+
+const path = "files/roommate.txt";
+const pathjson = "files/roommate.json";
+
+// const petro = require("fs");
+
 
 let usuarios = [];
-
+// console.log("hola")
 //creamos la llamada a la api
-const registerUser = async () =>{
-    const response = await axios.get('https://randomuser.me/api/');
-    const user = response.data.results[0];
-    console.log(user);
 
-    //la forma de llamar los datos que se necesitan de los usuarios
-    const newUser = {
-        firstName: user.name.first,
-        lastName: user.name.last,
-        id: uuidv4().slice(0, 6), //esto va a limita el numero de 6 digitos del id
+// function nuevoRoommate(){
+    const registerUser = async () =>{
+        const response = await axios.get('https://randomuser.me/api/');
+        const user = response.data.results[0];
+        console.log(user);
+        
+        //la forma de llamar los datos que se necesitan de los usuarios
+        let newUser = {
+            firstName: user.name.first,
+            lastName: user.name.last,
+            id: uuid4().slice(0, 6), //esto va a limita el numero de 6 digitos del id
+        };
+        console.log(newUser)
+        usuarios.push(newUser);
+        console.log(usuarios)
+        
+        petro.appendFileSync(path, newUser.toString())
+        petro.writeFileSync(pathjson, JSON.stringify(usuarios));
+    
+        let result = petro.readFileSync(path, { enconding:"utf-8"});
+    
+        const fromFile= JSON.parse(result);
+    
+    
+        console.log(fromFile);
+    
+     
     };
-
-    // aqui empujamos o creamos al nuevo usuario
-    usuarios.push(newUser);
-};
-//json con fs.writefileSync
+// };
+export {registerUser}; 
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 import express from "express";
-
+import { registerUser } from "./roommate.js";
 const app = express();
 
+app.use(express.static("public"));
+
 app.get("/", (req, res)=>{
-    res.send("Hola Mundo")
+    res.send("index");
 });
 
+app.get("/registro",async(req, res)=>{
+    await registerUser();
+    res.send(`Usuario registrado`);
+});
 //llamado servidor
 app.listen(3000, ()=>{
     console.log("Servidor corriendo puerto 3000")
